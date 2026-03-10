b5774dd7788c4c0bd6e4cac918911d3ae4d128eb
Rasenkai <rasenkai99@gmail.com>
2026-03-10 21:37:47 +0530

    techpack: disp: Fixup a derp
    * Original cherry-pick derped a letter that was fixed up later
    
    Change-Id: I1544155d2db35d610a7f8482370733d8ee662629
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- qcom/opensource/display-drivers/msm/msm_atomic.c
+++ qcom/opensource/display-drivers/msm/msm_atomic.c
@@ -566,7 +566,7 @@ static void complete_commit(struct msm_commit *c)
 
 static void _msm_drm_commit_work_cb(struct kthread_work *work)
 {
-	struct msm_commit *commit = container_of(work, typeof(*ccommit),
+	struct msm_commit *commit = container_of(work, typeof(*commit),
 						 commit_work);
 
 	SDE_ATRACE_BEGIN("complete_commit");
